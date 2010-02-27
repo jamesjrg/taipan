54fdbaa023aa85ed4dc696ff0eec4e6a2c788894
James Gregory <james.jrg@gmail.com>
2010-02-27 21:43:12 +0000

    case sensitivity issues

---
+++ Bank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaiPan.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
