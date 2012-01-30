47d397baa6924007de7d66900dd701604a6694be
James Gregory <james.jrg@gmail.com>
2012-01-30 16:52:18 +0000

    remove accidentally added line

--- create.bat
+++ create.bat
@@ -1,5 +1,4 @@
 :: don't run create_db.sql, assume that normally it already exists
 echo starting batch creation
 sqlcmd -U taipan-rw -P fakepass -d TaiPan -S (local) -i create_schema.sql -i insert_data.sql
-sqlcmd -U taipan-rw -P fakepass -d TaiPan -S (local) -i create_schema.sql -i insert_data.sql
 echo batch creation finished
